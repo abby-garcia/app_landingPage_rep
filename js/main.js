3f0f06bf08f1a8c8971d0983b981ee1c393393b4
abby-garcia <acg361@gmail.com>
2016-06-29 20:32:05 -0400

    padding edits

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-        $('.header_list').toggle();
+$(document).scroll(function(){
+        // grabs the pixel value of how far the image
+        // is from the top of the page
+        var phoneTop = $('.features_pic').offset().top;
+        // we determined we want the animation to happen
+        // 240pixels before we get to the image
+        phoneTop -= 240;
+        // scrollTop tells us where we are on the page
+        // if we've gotten down to the phone, add the
+        // animation classes
+        if($(document).scrollTop() >= phoneTop){
+            $('.features_pic').addClass('animated bounceInLeft');        
+        }
     });
-
-
-    $('.features_pic').addClass('animated bounceInLeft');
-
-    // need to put onScroll
-
-});
